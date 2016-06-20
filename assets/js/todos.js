8983db5b5004fedd7ede166af01a3b99d7ea9123
coded5282 <edwardjchen@gmail.com>
2016-06-19 23:32:46 -0400

    basic functionality added

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,10 +1,20 @@
 // Check off specific todos by clicking
-$("li").click(function() {
+$("ul").on("click", "li", function() { // so we can click on lis that do not exist at the time
 	$(this).toggleClass("completed"); 
 });
 
 // Click on X to delete Todo
-$("span").click(function(event) {
-	alert("clicked on the X"); 
+$("ul").on("click", "span", function(event) { // ul exists, but not all spans will exist at time so formatted like this
+	$(this).parent().fadeOut(500,function() {
+		$(this).remove(); 
+	});
 	event.stopPropagation(); 
+});
+
+$("input[type='text']").keypress(function(event) {
+	if (event.which === 13) {
+		var todoText = $(this).val(); // grabbing input
+		$(this).val(""); 
+		$("ul").append("<li><span>X</span> " + todoText + "</li>"); 
+	} 
 });
